88f276bb598377783d62f6da79853ea8f59e5c3e
AJ Wong <ajwong408@gmail.com>
2021-11-29 15:01:37 -0800

    updated users model

--- chat_app/lib/models/chat_users_model.dart
+++ chat_app/lib/models/chat_users_model.dart
@@ -1,13 +1,13 @@
 class ChatUsers {
-  String name;
-  String messageText;
-  String imageURL;
-  String time;
-
   ChatUsers({
     required this.name,
     required this.messageText,
     required this.imageURL,
     required this.time,
   });
+
+  String name;
+  String messageText;
+  String imageURL;
+  String time;
 }
