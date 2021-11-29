768cad29e172f7032e78f48a02fb9e5c8daa690e
AJ Wong <ajwong408@gmail.com>
2021-11-29 15:02:32 -0800

    abasic home screen finished

--- chat_app/lib/screens/chat_page.dart
+++ chat_app/lib/screens/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/conversation_list.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/models/chat_users_model.dart';
 
@@ -9,62 +10,126 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  List<ChatUsers> chatUsers = [];
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL: "images/userImage1.jpeg",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "images/userImage2.jpeg",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Jorge Henry",
+        messageText: "Hey where are you?",
+        imageURL: "images/userImage3.jpeg",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "images/userImage4.jpeg",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "images/userImage5.jpeg",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in evening",
+        imageURL: "images/userImage6.jpeg",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "images/userImage7.jpeg",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "John Wick",
+        messageText: "How are you?",
+        imageURL: "images/userImage8.jpeg",
+        time: "18 Feb"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SafeArea(
-                    child: Padding(
-                        padding:
-                            const EdgeInsets.only(left: 16, right: 16, top: 10),
-                        child: Stack(
-                          children: <Widget>[
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: Container(
-                                    width: 32,
-                                    height: 32,
-                                    decoration: const BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        image: DecorationImage(
-                                            fit: BoxFit.fill,
-                                            image: NetworkImage(
-                                                "https://i.imgur.com/BoN9kdC.png"))))),
-                            const Align(
-                                alignment: Alignment.center,
-                                child: Expanded(
-                                    child: Center(
-                                        child: Text(
-                                  "Chats",
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                )))),
-                            Align(
-                                alignment: Alignment.topRight,
-                                child: Container(
-                                  padding: const EdgeInsets.only(
-                                      left: 8, right: 8, top: 2, bottom: 2),
-                                  height: 30,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(30),
-                                    color: Colors.blue[50],
-                                  ),
-                                  child: const Icon(
-                                    Icons.create_rounded,
-                                    color: Colors.blue,
-                                    size: 20,
-                                  ),
-                                )),
-                          ],
-                        )))
-              ])),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SafeArea(
+                child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, top: 10),
+                    child: Stack(
+                      children: [
+                        const Align(
+                            alignment: Alignment.topLeft,
+                            child: CircleAvatar(
+                              radius: 16,
+                              backgroundImage: NetworkImage(
+                                  "https://i.imgur.com/BoN9kdC.png"),
+                            )),
+                        const Align(
+                            alignment: Alignment.center,
+                            child: Expanded(
+                                child: Center(
+                                    child: Text(
+                              "Messages",
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            )))),
+                        Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  left: 8, right: 8, top: 2, bottom: 2),
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.blue[50],
+                              ),
+                              child: const Icon(
+                                Icons.create_rounded,
+                                color: Colors.blue,
+                                size: 20,
+                              ),
+                            )),
+                      ],
+                    ))),
+            Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(color: Colors.grey.shade600),
+                        prefixIcon: Icon(Icons.search,
+                            color: Colors.grey.shade600, size: 20),
+                        filled: true,
+                        fillColor: Colors.grey.shade100,
+                        contentPadding: const EdgeInsets.all(8),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide:
+                                BorderSide(color: Colors.grey.shade100))))),
+            ListView.builder(
+                itemCount: chatUsers.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(top: 16),
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return ConversationList(
+                    name: chatUsers[index].name,
+                    messageText: chatUsers[index].messageText,
+                    imageURL: chatUsers[index].imageURL,
+                    time: chatUsers[index].time,
+                    isMessageRead: (index == 0 || index == 3) ? true : false,
+                  );
+                }),
+          ])),
     );
   }
 }
